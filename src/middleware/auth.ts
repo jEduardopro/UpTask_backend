c304d06aa2d9ea88eca12773bec7090d9fa2a051
Jesus Eduardo <lalo.codein22@gmail.com>
2023-10-02 08:43:51 -0600

    catch errors of verification token in the auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Response } from "express";
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import User from "../models/User";
 import { AuthReq, HttpStatusCode } from "../types";
 
@@ -19,22 +19,31 @@ const auth = async (req: AuthReq, res: Response, next: NextFunction) => {
 		})
 	}
 
-	const decoded = jwt.verify(token, process.env.JWT_SECRET)
-	if (typeof decoded === 'string') {
-		console.log('decoded is string');
-		return res.status(HttpStatusCode.UNAUTHORIZED).json({
-			message: 'Unauthorized'
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
+	
+		if (typeof decoded === 'string') {
+			console.log('decoded is string');
+			return res.status(HttpStatusCode.UNAUTHORIZED).json({
+				message: 'Unauthorized'
+			})
+		}
+	
+		const user = await User.findById(decoded.id).select("-password -confirmed_at -token -updatedAt -createdAt")
+		if (!user) {
+			return res.status(HttpStatusCode.UNAUTHORIZED).json({
+				message: 'Unauthorized'
+			})
+		}
+		req.user = { id: user.id, name: user.name, email: user.email}	
+		next()
+		
+	} catch (error) {
+		res.status(HttpStatusCode.UNAUTHORIZED).json({
+			message: 'Unauthorized',
 		})
 	}
 
-	const user = await User.findById(decoded.id).select("-password -confirmed_at -token -updatedAt -createdAt")
-	if (!user) {
-		return res.status(HttpStatusCode.UNAUTHORIZED).json({
-			message: 'Unauthorized'
-		})
-	}
-	req.user = { id: user.id, name: user.name, email: user.email}	
-	next()
 }
 
 export default auth
